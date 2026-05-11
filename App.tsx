5ae5d3eb923af9a4231d1260cc05b6fa31124cb3
Aron <aronjohnbarrameda18@gmail.com>
2026-05-11 13:18:07 +0000

    commit

--- App.tsx
+++ App.tsx
@@ -126,7 +126,7 @@ export default function App() {
       category: "Long/Short Form Videos",
       title: "Cinematic Rhythm",
       description: "Music Video • Direction & Pacing",
-      src: "https://vimeo.com/1168030393?share=copy&fl=sv&fe=ci"
+      src: "/assets/Marketing.mp4"
     },
     {
       category: "Long/Short Form Videos",
