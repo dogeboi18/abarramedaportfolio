93c116d5f6d395e725c97fa474be6991db4e3d30
Aron <aronjohnbarrameda18@gmail.com>
2026-05-17 00:05:38 +0800

    Fix file name case for video source

--- App.tsx
+++ App.tsx
@@ -126,7 +126,7 @@ export default function App() {
       category: "Long/Short Form Videos",
       title: "Cinematic Rhythm",
       description: "Music Video • Direction & Pacing",
-      src: "public/assets/car_edit.mp4"
+      src: "public/assets/CarEdit.mp4"
     },
     {
       category: "Long/Short Form Videos",
