9fc0e9bdbeb6d420515b284d3faf631b42432ca2
Aron <aronjohnbarrameda18@gmail.com>
2026-05-16 23:57:11 +0800

    Update video source in App.tsx

--- App.tsx
+++ App.tsx
@@ -126,7 +126,7 @@ export default function App() {
       category: "Long/Short Form Videos",
       title: "Cinematic Rhythm",
       description: "Music Video • Direction & Pacing",
-      src: "public/assets/car edit.mp4"
+      src: "public/assets/Marketing.mp4"
     },
     {
       category: "Long/Short Form Videos",
